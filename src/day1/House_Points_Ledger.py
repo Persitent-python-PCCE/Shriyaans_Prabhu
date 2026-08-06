806fc6cd967d2487e7049501e2594a8c48589c8d
prabushriyaans <prabhudrivewwe@gmail.com>
2026-08-06 09:51:30 +0530

    House_Points_Ledger.py updated all files upto date

--- src/day1/House_Points_Ledger.py
+++ src/day1/House_Points_Ledger.py
@@ -1,2 +1,16 @@
-def award_points(house,points=10,reason="general excellence" ,ledger=None):
+def award_points(house,points=20,reason="general excellence" ,ledger=None):
+    if ledger is None:
+        ledger={}
+    if house in ledger:
+        ledger[house]+=points
+    else:
+        ledger[house]=points
+    print(f"{house} +{points} ({reason}) total {ledger[house]}")
+    return ledger
+led = award_points("Gryffindor")
+led = award_points("Gryffindor", 50,
+"defeating a troll", led)
+led = award_points("Slytherin", 30,
+ledger=led)
+print(f"Final ledger:{led}")
     
