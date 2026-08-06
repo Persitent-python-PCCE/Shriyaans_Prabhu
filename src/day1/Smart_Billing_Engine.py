01e5251a3e1785a6050ad2df40e0a00e9c72b288
prabushriyaans <prabhudrivewwe@gmail.com>
2026-08-06 09:07:11 +0530

    Smart_Billing_Engine.py code updated.

--- src/day1/Smart_Billing_Engine.py
+++ src/day1/Smart_Billing_Engine.py
@@ -1,5 +1,5 @@
 ls=[("Masala Chai", 3, 20), ("Samosa", 2,
 15), ("Green Tea", 1, 30)]
 r1=list(map(lambda Q: (Q[1]* Q[2]*1.05),ls))
-print(r1)
-print(sum(r1))
+print(f"Line totals (incl. GST): {r1}")
+print(f"Grand total: {sum(r1)}")
